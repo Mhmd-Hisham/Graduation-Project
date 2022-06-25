41f14e69e241774876f2d3ab066fd5a110cf0963
Mhmd-Hisham <Mohamed00Hisham@gmail.com>
2022-06-25 05:24:15 +0200

    June 25 - 2022 Updates from Google Drive

---
+++ Utils/dialogue_utils.py
@@ -0,0 +1,101 @@
+import os
+
+import numpy as np
+import Utils.helperFunctions as helperFunctions
+
+get_max_category = lambda d: list(sorted(d.items(), reverse=True))[0][0]
+
+def get_n_dialogues(df):
+    return df['dialogue'].shape[0]
+
+def get_turns_per_dialogue(df):
+    """
+        Returns the turns per dialogue in the given dataframe.
+        The dataframe must have a 'dialogue' column.
+
+        Returns a vector.
+    """
+    return df['dialogue'].astype(str).apply(lambda x: x.split("\n"))
+
+def get_avg_turns_per_dialogue(df):
+    """
+        Returns the average number of turns per dialogue in the given dataframe.
+        The dataframe must have a 'dialogue' column.
+
+        Returns a scalar value.
+    """
+    n_dialogues = get_n_dialogues(df)
+    n_turns_per_dialog = get_turns_per_dialogue(df).apply(len)
+    return np.sum(n_turns_per_dialog)/n_dialogues
+
+def get_n_turns_per_dialogue(df):
+    """
+        Returns the number of turns per dialogue in the given dataframe.
+        The dataframe must have a 'dialogue' column.
+
+        Returns a vector.
+    """
+    turns_per_dialog = get_turns_per_dialogue(df)
+    n_turns_per_dialog = turns_per_dialog.apply(len)
+    return n_turns_per_dialog
+
+def get_n_words_per_dialogue(df):
+    """
+        Returns the number of words per dialogue in the given dataframe.
+        The dataframe must have a 'dialogue' column.
+
+        Returns a vector.
+    """
+    words_per_dialog = df['dialogue'].apply(lambda x: x.split())
+    n_words_per_dialog = words_per_dialog.apply(len)
+
+    return n_words_per_dialog
+
+def get_n_turns(df):
+    """
+        Returns the number of turns in the 'dialogue' column of the given dataframe.
+        Returns a scalar value.
+    """
+    return np.sum(get_n_turns_per_dialogue(df))
+
+def get_n_words(df):
+    """
+        Returns the number of words in the 'dialogue' column of the given dataframe.
+        Returns a scalar value.
+    """
+    return np.sum(get_n_words_per_dialogue(df))
+
+def get_avg_words_per_turn(df):
+    """
+        Returns the average number of words per turn in the given dataframe.
+        The dataframe must have a 'dialogue' column.
+
+        Returns a scalar value.
+    """
+    n_words = get_n_words(df)
+    n_turns = get_n_turns(df)
+    return n_words/n_turns
+
+
+def get_stats(df):
+    stats = dict()
+
+    stats['# dialogues'] = get_n_dialogues(df)
+    stats['# utterances'] = get_n_turns(df)
+    stats['avg # turns in a dialogue'] = get_avg_turns_per_dialogue(df)
+    stats['avg # words in a turn'] = get_avg_words_per_turn(df)
+
+    return stats
+
+def get_classification_from_history(path_to_history, df, dtype='U60'):
+    topics = np.array(["unclassified"]*df.shape[0], dtype=dtype)
+
+    history = dict()
+    if os.path.exists(path_to_history):
+        history = helperFunctions.read_json(path_to_history)
+        classified_from_history = np.array(list(history.keys())).astype(int)
+        classified_from_history_values = np.array(list(map(get_max_category, 
+                                                           history.values())))
+        topics[classified_from_history] = classified_from_history_values
+
+    return topics, history
---
+++ Utils/helperFunctions.py
@@ -0,0 +1,165 @@
+import os
+import json
+import zipfile
+import functools
+
+import requests
+import pandas as pd
+import numpy as np
+
+print = functools.partial(print, flush=True)
+
+def get_filename_from_headers(url, headers):
+    # print(headers.items())
+    try:
+        filename = headers["content-disposition"]
+        idx = filename.find("=")
+        filename = filename[idx+1:]
+    except Exception as E:
+        filename = url.split("/")[-1]
+
+    return filename
+
+def download_file(url, dest, override=False, create_dirs=True):
+    filename = requests.head(url, allow_redirects=True).url.split("/")[-1]
+
+    if create_dirs and not os.path.exists(dest):
+        os.makedirs(dest)
+
+    # filename = get_filename_from_headers(url, res.headers)
+    filepath = os.path.join(dest, filename)
+
+    if (not os.path.exists(filepath)) or (override):
+        if (override):
+            print(f"File '{filename}' exists! Overriding.. ", end="")
+
+        else:
+            print(f"Downloading '{filename}'.. ", end="")
+
+            with open(filepath, "wb+") as fh:
+                res = requests.get(url)
+
+                if (res.status_code != 200):
+                    print(f"Error! Couldn't download from this url={url}")
+                    return
+
+                fh.write(res.content)
+            print("Done!")
+    else:
+        print(f"File '{filename}' exists! Enable override to override it.")
+    return filename
+
+def download_from_list(urls, dest, override=False, create_dirs=True):
+    files = []
+    if (isinstance(urls, str)):
+        urls = parse_urls(urls)
+        
+    for url in urls:
+        filename = download_file(url, dest, override)
+        files.append(filename)
+    
+    return files
+
+def parse_urls(urls):
+    return urls.strip().split()
+
+def unzip(path_to_zip, target_dir, extract_directly=False):
+    print(f"Unzipping '{path_to_zip}'.. ", end="", flush=True)
+
+    # get the absolute paths
+    path_to_zip = os.path.abspath(path_to_zip)
+    target_dir = os.path.abspath(target_dir)
+    parent_dir = os.path.splitext(path_to_zip)[0].split(os.sep)[-1] + os.sep
+
+    # target dir 
+    with zipfile.ZipFile(path_to_zip, "r") as zip_ref:
+        # create a folder with the same name as the zip file
+        # only if requested
+        if not extract_directly:
+            names = zip_ref.namelist()
+            
+            if names[0] != parent_dir:
+                target_dir = os.path.join(target_dir, parent_dir)
+
+        zip_ref.extractall(target_dir)
+    
+    print("Done!")
+
+    return os.path.join(target_dir, parent_dir)
+
+def unrar(path_to_rar, target_dir):
+    import patoolib
+    print(f"Extracting '{path_to_rar}'.. ", end="", flush=True)
+    parent_dir = os.path.splitext(path_to_rar)[0]
+    if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
+    
+    target_dir = os.path.join(target_dir, parent_dir)
+    patoolib.extract_archive(path_to_rar, outdir=target_dir)    
+    print("Done!")
+
+    return target_dir
+
+def read_jsonl(path_to_jsonl_file):
+    json_objs = []
+
+    # load the file
+    with open(path_to_jsonl_file, "r") as fh:
+
+        # split the lines
+        lines = fh.read().splitlines()
+
+        # parse each line as a json object
+        for line in lines:
+            json_objs.append(json.loads(line))
+
+    # return a list of json objects
+    return json_objs
+
+def read_json(path_to_json_file):
+    with open(path_to_json_file, "r") as fh:
+        data = json.load(fh)
+    
+    return data
+
+def jsonl_to_df(json_objs):
+    """ 
+        Takes a list of json objects (from a jsonl file)
+        and converts it to a pandas dataframe.
+    """
+
+    # get a set of all avalible dictionary keys in the json objects
+    keys = set()
+    for row in json_objs:
+        keys |= set(list(row.keys()))
+
+    # convert to a list to follow a specific order (but a random order)
+    keys = list(keys)
+
+    # extract the data from eact dictionary and fill nulls
+    list_of_data = [[d.get(key, np.nan) for key in keys] for d in json_objs]
+
+    # create the dataframe and set the column names
+    df = pd.DataFrame(data=list_of_data)
+    df.columns = keys
+
+    return df
+
+def save_as_json(obj, filename, destination):
+    with open(os.path.join(destination, filename), "w+") as fh:
+        json.dump(obj, fh)
+
+def dense_to_sparse(list_of_values :list, mapper: dict):
+    columns = list(mapper.values())
+    result = np.zeros((len(list_of_values), len(columns)))
+
+    for row, value in enumerate(list_of_values):
+        col = columns.index(mapper[value])
+        result[row, col] = 1
+
+    return pd.Dataframe(result, columns)
+
+
+
+
+
